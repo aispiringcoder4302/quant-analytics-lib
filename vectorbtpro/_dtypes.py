6f6a2e9c31cb3ea5c236f687a90929a9c82668eb
Oleg Polakow <olegpolakow@gmail.com>
2025-05-28 00:23:00 +0200

    Update _dtypes.py

--- vectorbtpro/_dtypes.py
+++ vectorbtpro/_dtypes.py
@@ -28,14 +28,14 @@ __pdoc__ = {}
 int_ = settings["numpy"]["int_"]
 """_"""
 
-__pdoc__["int_"] = "Default integer data type retrieved from `vectorbtpro._settings.numpy`."
+__pdoc__[int_.__name__] = "Default integer data type retrieved from `vectorbtpro._settings.numpy`."
 if np.issubdtype(int_, np.integer):
-    __pdoc__["int_.bit_count"] = False
+    __pdoc__[f"{int_.__name__}.bit_count"] = False
 
 float_ = settings["numpy"]["float_"]
 """_"""
 
-__pdoc__["float_"] = "Default floating point data type retrieved from `vectorbtpro._settings.numpy`."
+__pdoc__[float_.__name__] = "Default floating point data type retrieved from `vectorbtpro._settings.numpy`."
 if np.issubdtype(float_, np.floating):
-    __pdoc__["float_.as_integer_ratio"] = False
-    __pdoc__["float_.is_integer"] = False
+    __pdoc__[f"{float_.__name__}.as_integer_ratio"] = False
+    __pdoc__[f"{float_.__name__}.is_integer"] = False
